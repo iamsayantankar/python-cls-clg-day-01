2f40bab73e5aef612265810e92bcc50480648a26
sayan <sayantankar01@gmail.com>
2022-09-21 13:49:17 +0530

    Update README.md

--- i_cls.py
+++ i_cls.py
@@ -9,3 +9,12 @@ for i in range(1, 6):
 
 for i in range(1, 6):
     print(" " * (5 - i) + a[i - 1:-6:-1] + a[1:i])
+
+for i in range(1, 6):
+    if i == 1 or i==5:
+        print(a[:i])
+
+    else:
+        print(a[0], end="")
+        print(" " * (i - 1), end="")
+        print(a[i - 1])
